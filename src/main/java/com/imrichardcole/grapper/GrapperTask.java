0f5fdd7b748788523ee8eb00ddddce901566cf84
Richard Cole <richcoleuk@googlemail.com>
2014-05-23 21:08:40 +0100

    tweaks

--- src/main/java/com/imrichardcole/grapper/GrapperTask.java
+++ src/main/java/com/imrichardcole/grapper/GrapperTask.java
@@ -1,20 +1,33 @@
 package com.imrichardcole.grapper;
 
 import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.TaskAction;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class GrapperTask extends DefaultTask {
 
-    //@Input
-    String grabberName;
+    @Input
+    String mainClassName;
 
     @TaskAction
-    public void fooTask() {
-        System.out.println("Hello from fooTask " + grabberName);
+    public void grapperTask() throws IOException {
+        System.out.println("mainClassName = " + mainClassName);
+        Properties wrapper = new Properties();
+        final FileOutputStream conf = new FileOutputStream("wrapper.conf");
+        wrapper.setProperty("wrapper.java.mainclass", mainClassName);
+        wrapper.setProperty("wrapper.java.command", "java");
+
+
+        wrapper.store(conf, null);
     }
 
-    public void grabberName(String test) {
-        grabberName = test;
+    public void setMainClassName(String mainClassName) {
+        this.mainClassName = mainClassName;
     }
 
 }
