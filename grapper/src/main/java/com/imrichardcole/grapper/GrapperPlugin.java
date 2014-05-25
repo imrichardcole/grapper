efa52b136479d46682b9a32fd65a542a7227ba94
Richard Cole <richcoleuk@googlemail.com>
2014-05-25 18:00:22 +0100

    tidy up before pushing to github

--- grapper/src/main/java/com/imrichardcole/grapper/GrapperPlugin.java
+++ grapper/src/main/java/com/imrichardcole/grapper/GrapperPlugin.java
@@ -1,5 +1,6 @@
 package com.imrichardcole.grapper;
 
+import com.imrichardcole.grapper.extension.GrapperJavaExtension;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
@@ -7,9 +8,10 @@ public class GrapperPlugin implements Plugin<Project> {
 
     @Override
     public void apply(Project project) {
-        final GrapperTask grabberTask = project.getTasks().create("grapper", GrapperTask.class);
-        grabberTask.setGroup("Grapper");
-        grabberTask.setDescription("generate a wrapper script");
+        final GrapperTask grapperTask = project.getTasks().create("grapper", GrapperTask.class);
+        grapperTask.setGroup("Grapper");
+        grapperTask.setDescription("generate a wrapper script");
+        grapperTask.getExtensions().add("java", GrapperJavaExtension.class);
     }
 
 }
--- grapper/src/main/java/com/imrichardcole/grapper/GrapperTask.java
+++ grapper/src/main/java/com/imrichardcole/grapper/GrapperTask.java
@@ -1,33 +1,61 @@
 package com.imrichardcole.grapper;
 
+import com.imrichardcole.grapper.extension.GrapperJavaExtension;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.TaskAction;
 
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 import java.util.Properties;
 
 public class GrapperTask extends DefaultTask {
 
-    @Input
     String mainClassName;
+    List<String> additionals;
 
     @TaskAction
     public void grapperTask() throws IOException {
-        System.out.println("mainClassName = " + mainClassName);
         Properties wrapper = new Properties();
         final FileOutputStream conf = new FileOutputStream("wrapper.conf");
         wrapper.setProperty("wrapper.java.mainclass", mainClassName);
         wrapper.setProperty("wrapper.java.command", "java");
-
-
+        handleAdditional(wrapper, additionals);
         wrapper.store(conf, null);
+        final GrapperJavaExtension java = getExtensions()
+                .getByType(GrapperJavaExtension.class);
+        System.out.println(java.getInitMemory());
+        System.out.println(java.getMaxMemory());
     }
 
+    private void handleAdditional(Properties wrapper,
+                                  List<String> additionals) {
+        int counter = 1;
+        for (String additional : additionals) {
+            wrapper.setProperty("java.wrapper.additional." + counter, additional);
+            counter++;
+        }
+    }
+
+    @Input
     public void setMainClassName(String mainClassName) {
         this.mainClassName = mainClassName;
+    }
+
+    @Input
+    public void setAdditional(List<String> additionals) {
+        this.additionals = additionals;
+    }
+
+    @Input
+    public void setInitMemory(int initMemory) {
+        System.out.println("initMemory = " + initMemory);
+    }
+
+    @Input
+    public void setMaxMemory(int maxMemory) {
+        System.out.println("maxMemory = " + maxMemory);
     }
 
 }
